4b095569e39e97930a1cf8fb10505beef26f3e57
pankajkoche <pankajkoche3399@gmail.com>
2022-05-27 09:16:09 -0700

    Update app.js

--- node-template-backend/Api/app.js
+++ node-template-backend/Api/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/testAPI',testAPI)
+app.use('/Api/getMessage',testAPI)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
