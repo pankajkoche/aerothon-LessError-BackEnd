4ab8b3cf05a7e350865d3e7e851f1eab52b5da3d
pankajkoche <pankajkoche3399@gmail.com>
2022-05-27 09:26:53 -0700

    api change and readme file added

--- Flask/Api/Task.py
+++ Flask/Api/Task.py
@@ -3,13 +3,13 @@ import logging as log
 class Task(Resource):
     def get(Self):
         log.debug("Inside GET Method")
-        return ("Message You are inside the GET method",200 ) # 200 is a success call
+        return ("hey!, You are Awesome. Now you are connected with backend. keep going",200 ) # 200 is a success call
     def post(Self):
         log.debug("Inside POST Method")
-        return {"Message":"You are inside the POST method"},200 
+        return {" POST method"},200 
     def put(Self):
         log.debug("Inside PUT Method")
-        return {"Message":"You are inside the PUT method"},200 
+        return {" PUT method"},200 
     def delete(Self):
         log.debug("Inside DELETE Method")
-        return {"Message":"You are inside the DELETE method"},200  
+        return {" DELETE method"},200  
---
+++ Flask/readme.md
@@ -0,0 +1,6 @@
+# Getting Started Falsk
+### Following are important packages that need to be installed for run this backend 
+1. Install Python updated version 
+2. pip install flask 
+3. pip install -U flask-cors
+4. pip install flask_restful
